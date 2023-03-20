5e8127f6409b0c3d40808e05ac14d3af57afba3c
Athira Parameswaran s <athira007p@gmail.com>
2023-03-20 14:23:10 +0530

    Add project files.

---
+++ ConsoleApp2/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Class1
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://www.weatherapi.com/docs/weather_conditions.json");
+            request.Headers.Add("Cookie", "SERVERUSED=web-3");
+            var response = await client.SendAsync(request);
+            var result = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(result);
+           
+
+        }
+    }
+}
